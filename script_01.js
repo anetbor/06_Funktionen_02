9435f247c8a2946d94fa636757ddbb2132987f21
Anet <anetborin@mail.com>
2022-01-20 11:46:52 +0100

    return a,b

--- script_01.js
+++ script_01.js
@@ -8,6 +8,54 @@
 4. Ausgabe in Konsole
 */
 
+//0
+
+//addition
+
+// output(add(2,3));
+function add(a,b) {
+  return a + b; 
+}
+
+//subtraction
+
+// output(sub(2,10));
+function sub(a,b) {
+    return a - b; 
+     }      
+
+//multiplication
+
+// output(mul(3,3));
+function mul(a,b) {
+    return a * b; 
+     }      
+
+//division
+
+output(div(3,0));
+function div(a,b) {
+   
+    if (b == 0) {
+        // output("keine Zahl")
+        return"Fehler";
+ 
+    } 
+    return a/b; 
+  }
+
+function output(outputData){ 
+    if (typeof outputData == "number") {
+        console.log("The result is: " + outputData);
+ 
+    } else {
+        console.log("Error:" +  outputData);
+    }
+// console.log(outputData);
+
+  }
+
+
 
 
 
@@ -18,22 +66,23 @@
  wir gehen in den Wienerwald. 
 */
 
-/*
- servieren(anrichten(gemuesekoch(),saucenkoch()));
 
-function servieren(teller) {
-    console.log("Einmal einen " + teller +  ", Bitte sehr!")
-}
+//  servieren(anrichten(gemuesekoch(),saucenkoch()));
 
-function anrichten(kartoffeln,soße) {
-    return "Teller mit: "+ kartoffeln + " " + soße;
-}
+// function servieren(teller) {
+//     console.log("Einmal einen " + teller +  ", Bitte sehr!")
+// }
 
-function gemuesekoch(){
-    return "gekochte Kartoffeln";
- }
+// function anrichten(kartoffeln,soße) {
+//     return "Teller mit: "+ kartoffeln + " " + soße;
+// }
+
+// function gemuesekoch(){
+//     return "gekochte Kartoffeln";
+//  }
  
- function saucenkoch() {
-     return "lecker Sauce";
- }
- */
+//  function saucenkoch() {
+//      return "lecker Sauce";
+//  }
+ 
+
